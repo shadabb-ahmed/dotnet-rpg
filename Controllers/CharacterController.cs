46bf729450c2bd057a43c4fb13756fb480160ad3
Ahmed shadab <shadabb2000@gmail.com>
2022-11-24 12:07:51 +0530

    Post and Get Method Added

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -6,11 +6,31 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character Knight = new Character();
+        private static List<Character> characters = new List<Character>{
+            new Character(),
+            new Character{Name = "sam"}
+        
+        };
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> GetCharacterList() {
+            return Ok(characters);
+        }
 
         [HttpGet]
-        public ActionResult<Character> Get() {
-            return Ok(Knight);
+        public ActionResult<Character> GetSingleCharacter() {
+            return Ok(characters[0]);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetCharacterById(int id) {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> GetCharacterById(Character character) {
+            characters.Add(character);
+            return Ok(characters);
         }
     }
 }
